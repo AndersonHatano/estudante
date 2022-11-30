c356e395cea6759fd4436fbdeb3fd9d511bcc61a
Anderson Hatano <49458366+AndersonHatano@users.noreply.github.com>
2022-11-30 15:05:06 -0300

    teste

--- src/component/estudante/Carteiraluquinha.js
+++ src/component/estudante/Carteiraluquinha.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CarteiraLucas from '../estudante/luquinha.jpg'
 import LogoClubeDoEstudante from '../estudante/clubeDoEstudante2.jpg'
+import qrCodeLuquinha from '../estudante/qrCodeLuquinha.png'
 import '../estudante/Carteira.css'
 
 class CarteiraLuquinha extends Component {
@@ -43,6 +44,16 @@ class CarteiraLuquinha extends Component {
                     <div class="columns is-mobile">
                         <div class="column is-half is-offset-one-quarter">
                             <h2>QRCode de validação:</h2>
+                        </div>
+                    </div>
+
+                    <div class="columns is-mobile">
+                        <div class="column is-half is-offset-one-quarter">
+
+                            <div  class="block">
+                                <img src={qrCodeLuquinha} alt='qr code luquinha'/>
+                            </div>
+
                         </div>
                     </div>
                     
