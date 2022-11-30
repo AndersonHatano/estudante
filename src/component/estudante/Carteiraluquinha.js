010561405cb5874c5acdcaf8b98f63360ad0ef12
Anderson Hatano <49458366+AndersonHatano@users.noreply.github.com>
2022-11-30 02:22:03 -0300

    teste

--- src/component/estudante/Carteiraluquinha.js
+++ src/component/estudante/Carteiraluquinha.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CarteiraLucas from '../estudante/luquinha.jpg'
 import LogoClubeDoEstudante from '../estudante/clubeDoEstudante2.jpg'
+import '../estudante/Carteira.css'
 
 class CarteiraLuquinha extends Component {
 
@@ -9,35 +10,42 @@ class CarteiraLuquinha extends Component {
 
         return (
 
-            
-            <div class="column is-half is-offset-one-quarter">
+            <div>
+                <div class="column is-half is-offset-one-quarter">
 
-                <div class="columns is-mobile">
-                    <div class="column is-half is-offset-one-quarter">
+                    <div class="columns is-mobile">
+                        <div class="column is-half is-offset-one-quarter">
 
-                        <div  class="block">
-                            <img src={LogoClubeDoEstudante} alt='logo'/>
+                            <div  class="block">
+                                <img src={LogoClubeDoEstudante} alt='logo'/>
+                            </div>
+
                         </div>
-
-                        <div class="block">
-                            <button class="button is-danger is-rounded">MENU PRINCIPAL</button>
-                        </div>
-
-                        <div class="block">
-                            <button class="button is-danger is-rounded">VER PRÉVIA DA CIE</button>
-                        </div>
-
                     </div>
-                </div>
 
-                <div class="block">
-                    <img src={CarteiraLucas} alt='carteirinha lucas'/>
+                    <div class="block">
+                        <button class="button is-rounded">MENU PRINCIPAL</button>
+                    </div>
+
+                    <div class="block">
+                        <button class="button is-rounded">VER PRÉVIA DA CIE</button>
+                    </div>
+
+                        
                 </div>
 
                 <div class="column is-half is-offset-one-quarter">
-                    <h2>QRCode de validação:</h2>
+
+                    <div class="block">
+                        <img src={CarteiraLucas} alt='carteirinha lucas'/>
+                    </div>
+
+                    <div class="column is-half is-offset-one-quarter">
+                        <h2>QRCode de validação:</h2>
+                    </div>
+                    
                 </div>
-                
+                    
             </div>
 
         );
