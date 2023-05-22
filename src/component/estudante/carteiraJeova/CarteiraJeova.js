978af99f04c3706498ca4d6a2d8ce266e16f8ed9
Anderson Hatano <49458366+AndersonHatano@users.noreply.github.com>
2023-05-22 14:51:55 -0300

    Atualizando Jeova

--- src/component/estudante/carteiraJeova/CarteiraJeova.js
+++ src/component/estudante/carteiraJeova/CarteiraJeova.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import carteiraJeova from '../carteiraJeova/carteiraJeova.png'
+import carteiraJeova from '../carteiraJeova/carteirinhaJeova.JPG'
 import LogoClubeDoEstudante from '../clubeDoEstudante2.jpg'
 import qrCodeJeova from '../carteiraJeova/qrCodeJeova.png'
 import { Redirect } from 'react-router-dom'
